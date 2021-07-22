22cfba1b507b4591566b30716d6b798d9f75b592
RONIT SINGH <ronitsingh7003@gmail.com>
2021-07-22 15:15:26 +0530

    music added

--- JavaScripts/EnquireNow.js
+++ JavaScripts/EnquireNow.js
@@ -1,6 +1,7 @@
 const enquiry_button = document.querySelector('#home-enquiry-button');
 const enquiry_wrapper = document.querySelector('.enquiry-wrapper');
 const close = document.querySelector('.close');
+const enquire_submit = document.querySelector('.enquire-submit');
 
 enquiry_button.addEventListener('click', function(){
     enquiry_openModal();
@@ -9,7 +10,9 @@ enquiry_button.addEventListener('click', function(){
 close.addEventListener('click', function(){
     enquiry_closeModal();
 });
-
+enquire_submit.addEventListener('click' , function(){
+    enquiry_closeModal();
+})
 enquiry_wrapper.addEventListener('click', function(e){
     if(e.target !== this) return;
     enquiry_closeModal();
@@ -26,3 +29,17 @@ function enquiry_openModal(){
 function enquiry_closeModal() {
     enquiry_wrapper.classList.remove('enquiry-active');
 }
+
+
+
+// var audio = document.getElementsByClassName('.enquire-submit');
+// var count = 0;
+// function playpause(){
+//     if(count==0){
+//         count=1;
+//         audio.play();
+//     }else{
+//         count=0;
+//         // audio.pause();
+//     }
+// }
